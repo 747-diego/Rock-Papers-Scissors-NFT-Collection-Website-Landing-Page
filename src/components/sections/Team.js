a087ca02ddb4d9232c1ce938b05709ed685d30dc
747-diego <725@holbertonschool.com>
2022-03-20 20:18:06 -0400

    [UPDATED]

--- src/components/sections/Team.js
+++ src/components/sections/Team.js
@@ -142,15 +142,15 @@ const Team = () => {
     <ConfettiComponent  /> </Suspense>
       <Title>Team</Title>
       <Container>
-        <MemberComponent img={img1}  name="SKYBLAZE" position="founder" />
-        <MemberComponent img={img2}  name="MEGNUM" position="Co-Founder" />
-        <MemberComponent img={img3}  name="MONKEY KING" position="Director" />
-        <MemberComponent img={img4}  name="BLACK PANTHER" position="manager" />
-        <MemberComponent img={img5}  name="DEATHSTROKE" position="artist" />
-        <MemberComponent img={img6}  name="LAZY KONG" position="social media manager" />
-        <MemberComponent img={img7}  name="CYBER PUNK" position="Blockchain Specialist" />
-        <MemberComponent img={img8}  name="MONK" position="Web3 Developer" />
-        <MemberComponent img={img9}  name="DESIGNER" position="Graphic Designer" />
+        <MemberComponent img={img1}  name="THE BRAINS" position="founder" />
+        <MemberComponent img={img2}  name="THE CONDUCTOR" position="Co-Founder" />
+        <MemberComponent img={img3}  name="THE GO-TO GUY" position="Director" />
+        <MemberComponent img={img4}  name="THE OTHER GUY" position="manager" />
+        <MemberComponent img={img5}  name="THE MICHELANGELO" position="artist" />
+        <MemberComponent img={img6}  name="THE ASSITANT" position="social media manager" />
+        <MemberComponent img={img7}  name="THE CYBER PUNK" position="Blockchain Specialist" />
+        <MemberComponent img={img8}  name="THE DEV" position="Web3 Developer" />
+        <MemberComponent img={img9}  name="THE ARTIST" position="Graphic Designer" />
         
 
       </Container>
